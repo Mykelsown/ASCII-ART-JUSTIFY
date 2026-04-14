dfab0c4677dd95a93198356b5f479137abad1ab7
micsamuel <mykelsown@gmail.com>
2026-04-14 09:58:04 +0100

    update: added logic for printing usage message if user passes in an incorrect argument

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	asciiart "asciiartjustify/MethodsAndTesting"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
@@ -14,14 +15,13 @@ func main() {
 		formatType strings.Builder
 	)
 
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run . [OPTION] [STRING] [BANNER]\nExample: go run . --align=right something standard")
+	}
+
 	if len(os.Args) == 2 {
 		input.WriteString(os.Args[1])
-		if input.String() == "" {
-			return
-		}
-
 		fmt.Println(asciiart.FormatPrinter(input.String()))
-		return
 	} else if len(os.Args) == 4 {
 		position.WriteString(os.Args[1][8:])
 		input.WriteString(os.Args[2])
